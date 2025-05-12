876bd13eff2cfc02c27ba7cf2930cf250f55bc87
Ashish Kushwaha <ashish@wishfy.club>
2025-05-12 14:38:33 +0530

    Add a common package where we add the zod schema and the JWT-SECRET

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,5 +1,21 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { CreateUserSchema } from "@repo/common/types";
 
 const app = express();
+
+app.post("/signup", (req: Request, res: Response) => {
+  const data = CreateUserSchema.safeParse(req.body);
+  if (!data) {
+    res.json({
+      msg: "incorrent",
+    });
+    return;
+  }
+
+  // db call
+  res.json({
+    userId: 192,
+  });
+});
 
 app.listen(3000);

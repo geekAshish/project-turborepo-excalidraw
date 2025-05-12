ef611f0c3e3a61ca242220a4919374dc9044cb9f
Ashish Kushwaha <ashish@wishfy.club>
2025-05-12 14:48:04 +0530

    create route

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,5 +1,12 @@
 import express, { Request, Response } from "express";
-import { CreateUserSchema } from "@repo/common/types";
+import jwt from "jsonwebtoken";
+
+import {
+  CreateUserSchema,
+  SignInSchema,
+  CreateRoomSchema,
+} from "@repo/common/types";
+import { JWT_SECRET } from "@repo/backend-common/config";
 
 const app = express();
 
@@ -15,6 +22,38 @@ app.post("/signup", (req: Request, res: Response) => {
   // db call
   res.json({
     userId: 192,
+  });
+});
+app.post("/signin", (req: Request, res: Response) => {
+  const data = SignInSchema.safeParse(req.body);
+  if (!data) {
+    res.json({
+      msg: "incorrent",
+    });
+    return;
+  }
+
+  const userId = 1;
+  const token = jwt.sign({ userId }, JWT_SECRET);
+
+  // db call
+  res.json({
+    token,
+  });
+});
+
+app.post("/room", (req: Request, res: Response) => {
+  const data = CreateRoomSchema.safeParse(req.body);
+  if (!data) {
+    res.json({
+      msg: "incorrent",
+    });
+    return;
+  }
+
+  // db call
+  res.json({
+    roomId: 12,
   });
 });
 

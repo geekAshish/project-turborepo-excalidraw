b27c65af420af621a9109218e007f94bfc3798e0
Ashish Kushwaha <ashish@wishfy.club>
2025-05-14 09:51:01 +0530

    ws frontend

--- apps/web/app/room/[slug]/page.tsx
+++ apps/web/app/room/[slug]/page.tsx
@@ -1,7 +1,16 @@
+import axios from "axios";
 import React from "react";
+import { BACKEND_URL } from "../../config";
 // 2:15
-const ChatRoom = ({ params }: { params: { slug: string } }) => {
+
+async function getRoomId(slug: string) {
+  const response = await axios.get(`${BACKEND_URL}/room/${slug}`);
+  return response.data.id;
+}
+
+const ChatRoom = async ({ params }: { params: { slug: string } }) => {
   const slug = params.slug;
+  const roomId = await getRoomId(slug);
 
   return <div>ChatRoom {slug}</div>;
 };
---
+++ apps/web/components/ChatRoomClient.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+const ChatRoomClient = ({
+  messages,
+  id,
+}: {
+  messages: { message: string }[];
+  id: string;
+}) => {
+  return <div></div>;
+};
+
+export default ChatRoomClient;
